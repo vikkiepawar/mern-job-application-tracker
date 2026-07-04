0a42973fdec2400c09828e803cdee3ff06267c14
Vikram Pawar <pawarvikram2004@gmail.com>
2026-07-04 10:51:22 +0530

    Update MongoDB connection configuration

--- server/config/db.js
+++ server/config/db.js
@@ -6,6 +6,7 @@ dns.setDefaultResultOrder("ipv4first");
 const connectDB = async () => {
   try {
     console.log("Connecting to MongoDB...");
+    console.log(process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB Connected");
   } catch (error) {
